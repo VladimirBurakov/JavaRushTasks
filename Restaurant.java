f26642c7a0568c6deab87ad8702b7db5d9d70546
Katrin Burakova <katerinkabuva@gmail.com>
2020-03-30 16:14:30 +0300

    Commit2

--- Restaurant.java
+++ Restaurant.java
@@ -20,5 +20,6 @@ public class Restaurant {
         directorTablet.printCookWorkloading();
         directorTablet.printActiveVideoSet();
         directorTablet.printArchivedVideoSet();
+
     }
 }
